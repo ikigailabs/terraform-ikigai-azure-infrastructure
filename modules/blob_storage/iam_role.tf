2c201e751a75d7b871d068ee33c903ccfdf73457
Prashanth Mohan <pmohan@ikigailabs.io>
2024-07-22 15:16:11 -0400

    removed iam role from blob storage

--- modules/blob_storage/iam_role.tf
+++
@@ -1,9 +0,0 @@
-data "azuread_service_principal" "ikigai_service_principal" {
-  display_name = var.service_principal_name
-}
-
-resource "azurerm_role_assignment" "storage_contributor_assignment" {
-  scope                = azurerm_storage_account.development_storage_account.id
-  role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = data.azuread_service_principal.ikigai_service_principal.object_id
-}
