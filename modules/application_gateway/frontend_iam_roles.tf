d28d8b960206f8b681417164ccceb72be7a8a7f7
Prashanth Mohan <pmohan@ikigailabs.io>
2024-07-23 01:33:34 -0400

    iam refactor for app gateway

--- modules/application_gateway/frontend_iam_roles.tf
+++ modules/application_gateway/frontend_iam_roles.tf
@@ -4,50 +4,14 @@ resource "azurerm_role_assignment" "frontend_public_ip_reader_assignment" {
   principal_id       = data.azuread_service_principal.app_gateway_service_principal.object_id
 }
 
-resource "azurerm_role_definition" "frontend_app_gateway_contributor" {
-  name        = var.frontend_gateway_contributor_role_name
-  scope       = azurerm_application_gateway.frontend_application_gateway.id
-
-  permissions {
-    actions     = ["*"]
-    not_actions = [
-      "Microsoft.Authorization/*/Delete",
-      "Microsoft.Authorization/*/Write",
-      "Microsoft.Authorization/elevateAccess/Action",
-      "Microsoft.Blueprint/blueprintAssignments/write",
-      "Microsoft.Blueprint/blueprintAssignments/delete",
-      "Microsoft.Compute/galleries/share/action",
-      "Microsoft.Purview/consents/write",
-      "Microsoft.Purview/consents/delete"
-    ]
-  }
-}
-
 resource "azurerm_role_assignment" "frontend_app_gateway_contributor_assignment" {
   scope              = azurerm_application_gateway.frontend_application_gateway.id
-  role_definition_id = azurerm_role_definition.frontend_app_gateway_contributor.role_definition_resource_id
+  role_definition_name = "Contributor"
   principal_id       = data.azuread_service_principal.app_gateway_service_principal.object_id
-}
-
-resource "azurerm_role_definition" "frontend_app_gateway_identity_operator" {
-  name        = var.frontend_gateway_identity_reader_role_name
-  scope       = azurerm_user_assigned_identity.frontend_app_gateway_managed_identity.id
-
-  permissions {
-    actions = [
-      "Microsoft.ManagedIdentity/userAssignedIdentities/*/read",
-      "Microsoft.ManagedIdentity/userAssignedIdentities/*/assign/action",
-      "Microsoft.Authorization/*/read",
-      "Microsoft.Insights/alertRules/*",
-      "Microsoft.Resources/subscriptions/resourceGroups/read",
-      "Microsoft.Resources/deployments/*",
-      "Microsoft.Support/*"
-    ]
-  }
 }
 
 resource "azurerm_role_assignment" "frontend_app_gateway_identity_operator_assignment" {
   scope              = azurerm_user_assigned_identity.frontend_app_gateway_managed_identity.id
-  role_definition_id = azurerm_role_definition.frontend_app_gateway_identity_operator.role_definition_resource_id
+  role_definition_name = "Managed Identity Operator"
   principal_id       = data.azuread_service_principal.app_gateway_service_principal.object_id
 }
--- modules/application_gateway/ingress_iam_roles.tf
+++ modules/application_gateway/ingress_iam_roles.tf
@@ -4,50 +4,14 @@ resource "azurerm_role_assignment" "ingress_public_ip_reader_assignment" {
   principal_id       = data.azuread_service_principal.app_gateway_service_principal.object_id
 }
 
-resource "azurerm_role_definition" "ingress_app_gateway_contributor" {
-  name        = var.ingress_gateway_contributor_role_name
-  scope       = azurerm_application_gateway.ingress_application_gateway.id
-
-  permissions {
-    actions     = ["*"]
-    not_actions = [
-      "Microsoft.Authorization/*/Delete",
-      "Microsoft.Authorization/*/Write",
-      "Microsoft.Authorization/elevateAccess/Action",
-      "Microsoft.Blueprint/blueprintAssignments/write",
-      "Microsoft.Blueprint/blueprintAssignments/delete",
-      "Microsoft.Compute/galleries/share/action",
-      "Microsoft.Purview/consents/write",
-      "Microsoft.Purview/consents/delete"
-    ]
-  }
-}
-
 resource "azurerm_role_assignment" "ingress_app_gateway_contributor_assignment" {
   scope              = azurerm_application_gateway.ingress_application_gateway.id
-  role_definition_id = azurerm_role_definition.ingress_app_gateway_contributor.role_definition_resource_id
+  role_definition_name = "Reader"
   principal_id       = data.azuread_service_principal.app_gateway_service_principal.object_id
-}
-
-resource "azurerm_role_definition" "ingress_app_gateway_identity_operator" {
-  name        = var.ingress_gateway_identity_reader_role_name
-  scope       = azurerm_user_assigned_identity.ingress_app_gateway_managed_identity.id
-
-  permissions {
-    actions = [
-      "Microsoft.ManagedIdentity/userAssignedIdentities/*/read",
-      "Microsoft.ManagedIdentity/userAssignedIdentities/*/assign/action",
-      "Microsoft.Authorization/*/read",
-      "Microsoft.Insights/alertRules/*",
-      "Microsoft.Resources/subscriptions/resourceGroups/read",
-      "Microsoft.Resources/deployments/*",
-      "Microsoft.Support/*"
-    ]
-  }
 }
 
 resource "azurerm_role_assignment" "ingress_app_gateway_identity_operator_assignment" {
   scope              = azurerm_user_assigned_identity.ingress_app_gateway_managed_identity.id
-  role_definition_id = azurerm_role_definition.ingress_app_gateway_identity_operator.role_definition_resource_id
+  role_definition_id = "Managed Identity Operator"
   principal_id       = data.azuread_service_principal.app_gateway_service_principal.object_id
 }
