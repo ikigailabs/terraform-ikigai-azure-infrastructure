9b7a634086b7d9d24da4a1f4fb162011e8c96761
Prashanth Mohan <pmohan@ikigailabs.io>
2024-07-24 11:48:45 -0400

    added app id output for app insights

--- modules/application_insights/outputs.tf
+++ modules/application_insights/outputs.tf
@@ -2,3 +2,8 @@ output "application_insights_connection_string" {
   value = azurerm_application_insights.ikigai_insight.connection_string
   description = "Connection string for the Ikigai application insights instance"
 }
+
+output "application_insights_app_id" {
+  value = azurerm_application_insights.ikigai_insight.app_id
+  description = "App ID for the Ikigai application insights instance"
+}
