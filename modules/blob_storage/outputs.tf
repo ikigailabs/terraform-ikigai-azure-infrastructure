f8f704cfd3f024dc53bd3db65a3f864856ab4e8b
Prashanth Mohan <pmohan@ikigailabs.io>
2024-07-24 09:57:32 -0400

    added access key output for storage module

--- modules/blob_storage/outputs.tf
+++ modules/blob_storage/outputs.tf
@@ -1,4 +1,4 @@
-output "name" {
+output "storage_account_access_key" {
   value = azurerm_storage_account.development_storage_account.primary_access_key
   description = "Access key for the Ikigai storage account"
 }
